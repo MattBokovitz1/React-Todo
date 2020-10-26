22d02169658148feb064e7bb3d40843a4465f320
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2020-10-26 18:16:11 -0400

    finished mvp will style

---
+++ src/components/Todo.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Item = (props) => {
+  const handleClick = () => {
+    props.handleToggleItem(props.item.id);
+  };
+
+  return (
+    <div
+      onClick={handleClick}
+      className={`item${props.item.completed ? " completed" : ""}`}
+    >
+      <p>{props.item.task}</p>
+    </div>
+  );
+};
+
+export default Item;
